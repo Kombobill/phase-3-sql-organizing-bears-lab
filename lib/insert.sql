5b07e0ad9f70c32ce7d7d4acc0e5917fc1f68a56
Kombobill <118278282+Kombobill@users.noreply.github.com>
2023-02-21 23:05:42 +0300

    done assignment

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+ INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+ VALUES 
+  (1, 'Mr.Chocolate', 10, 'M', 'Black', 'Tasty', true)  
+  (2, 'Rowdy', 12, 'F', 'White', 'Tough', true) 
+  (3, 'Tabitha', 22, 'F', 'Black', 'Pretty', true) 
+  (4, 'Sergeant Brown', 22, 'M', 'Black', 'Huge', true) 
+  (5, 'Melissa', 19, 'F', 'Brown', 'Beauty', true) 
+  (6, 'Grinch', 20, 'M', 'Brown', 'Ugly', False) 
+  (7, 'Wendy', 22, 'F', 'Brown', 'Ugly', False) 
+  (NULL, 0, 22, 'F', 'Brown', 'Ugly', 2) 
